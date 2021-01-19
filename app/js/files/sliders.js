99559da0137e41c337fef06edb6fb95bc58bc4cb
second1996 <vital10012@gmail.com>
2021-01-19 16:06:06 +0200

    Update

---
+++ app/js/files/sliders.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.about-certificates-slider', {
+  slidesPerView: 4.5,
+  spaceBetween: 20,
+  freeMode: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+})
