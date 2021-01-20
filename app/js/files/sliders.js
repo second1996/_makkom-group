fffbfddd487326a60ed49c37140a127e6476998a
second1996 <vital10012@gmail.com>
2021-01-20 12:32:56 +0200

    Update

--- app/js/files/sliders.js
+++ app/js/files/sliders.js
@@ -1,11 +1,26 @@
-var mySwiper = new Swiper('.about-certificates-slider', {
-  slidesPerView: 4.5,
-  spaceBetween: 20,
-  freeMode: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-})
+if( $('.about-certificates-slider').length ) {
+  var aboutCertificates = new Swiper('.about-certificates-slider', {
+    slidesPerView: 4.5,
+    spaceBetween: 20,
+    freeMode: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.5,
+        spaceBetween: 10
+      },
+      576: {
+        slidesPerView: 2.5,
+      },
+      992: {
+        slidesPerView: 3.5,
+      },
+      1320: {
+        slidesPerView: 4.5,
+      }
+    }
+  })
+}
--- app/js/vendors.js
+++ app/js/vendors.js
@@ -1,4 +1,5 @@
 @@include('../../node_modules/jquery/dist/jquery.min.js') // jQuery
 @@include('../../node_modules/bootstrap/js/dist/util.js') // Bootstrap Util lib
 @@include('../../node_modules/bootstrap/js/dist/modal.js') // Bootstrap Modal
+@@include('../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js') // Fancybox
 @@include('libs/jquery.mask.min.js') // jQuery Mask input
